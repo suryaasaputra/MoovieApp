3388cf2b83259cfb6a002367d3ba10c7c2916acc
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-09-10 02:17:08 +0700

    fix logic & add dependecies

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import $ from "jquery";
 import moment from "moment";
+import Swal from "sweetalert2";
 import "../component/hero.js";
 import image from "../img/empty.svg";
 const hero = $("#hero");
 const keyword = $("#keyword");
-const caption = $(".section-title");
 const mainPage = $("#main");
 
 const main = () => {
@@ -20,32 +20,9 @@ const main = () => {
 		url: "https://api.themoviedb.org/3/trending/movie/week?api_key=a3d88eeb5f330c5238138a8a41be581e",
 		success: function (result) {
 			const movieList = result.results;
-
-			$.each(movieList, (num, movie) => {
-				const imgPath = "https://image.tmdb.org/t/p/original";
-				const dateConverted = moment(movie.release_date, "YYYY-MM-DD").format(
-					"LL"
-				);
-				$("#movieList").append(`
-					<div class="col-lg-4 movie-item">
-						<div class="card movie_card">
-							<img
-								src="${imgPath}${movie.backdrop_path}"
-								class="card-img-top img-fluid"
-								alt="..."
-							/>
-						<div class="card-body">
-								<h5 class="card-title">${movie.title}</h5>
-								<p class="card-text">
-										${movie.overview}
-								</p>
-								<span class="movie_info">Release Date :<b> ${dateConverted}</b></span>
-								<span class="movie_info float-end" >Rate: <b>${movie.vote_average}/10</b></span>
-						</div>
-						</div>
-					</div>`);
-			});
+			showMovieList(movieList);
 		},
+		error: () => showErrorMessage(),
 	});
 
 	$("#searchForm").on("submit", (event) => {
@@ -57,9 +34,6 @@ const main = () => {
 					mainPage.html("");
 					console.log("Not Found");
 					hero.remove();
-					// caption.html(
-					// 	`Empty result for :<span class="keyword"> ${keyword.val()}</span>`
-					// );
 					$("#movieList").html("");
 					mainPage.append(`
 					<section>
@@ -74,49 +48,71 @@ const main = () => {
 					</section>
 					`);
 				} else {
+					const searchResult = result.results;
 					console.log("search succes");
 					mainPage.html(`
 					<section>	
 						<div class="text-center p-3 d-flex">
-						<h3 class="section-title">Search result for:<span class="keyword"> ${keyword.val()}</span></h3>
+						<h3 class="section-title">Search results for:<span class="keyword"> ${keyword.val()}</span></h3>
 						</div>						
 						<div class="row px-4 justify-content-center movie-list"
 							id="movieList"
 						></div>
 					</section>
 					`);
+					showMovieList(searchResult);
+				}
+			},
+			error: () => {
+				showErrorMessage();
+			},
+		});
+	});
 
-					$("#movieList").html("");
-					const searchResult = result.results;
-					$.each(searchResult, (num, movie) => {
-						const imgPath = "https://image.tmdb.org/t/p/original";
-						const dateConverted = moment(
-							movie.release_date,
-							"YYYY-MM-DD"
-						).format("LL");
-						$("#movieList").append(`
-						<div class="col-lg-4 movie-item">
-							<div class="card movie_card">
+	const showMovieList = (movies) => {
+		$("#movieList").html("");
+		$.each(movies, (i, movie) => {
+			const imgPath = "https://image.tmdb.org/t/p/original";
+			const dateConverted = moment(movie.release_date, "YYYY-MM-DD").format(
+				"LL"
+			);
+			$("#movieList").append(`
+						<div class="d-flex flex-column col-lg-4 movie-item">
+							<div class="card h-100 movie_card">
 								<img
 									src="${imgPath}${movie.backdrop_path}"
 									class="card-img-top img-fluid"
-									alt="..."
+									alt="Movie poster unavaible "
 								/>
 							<div class="card-body">
 									<h5 class="card-title">${movie.title}</h5>
 									<p class="card-text">
 											${movie.overview}
 									</p>
-									<span class="movie_info">Release Date :<b> ${dateConverted}</b></span>
-									<span class="movie_info float-end" >Rate: <b>${movie.vote_average}/10</b></span>
+							</div>
+							<div class="card-bottom px-3 float-start ">
+							<div class="movie_info">Release Date :<b> ${dateConverted}</b></div>
+							<div class="movie_info" >Rating : <b>${movie.vote_average}/10</b></div>
 							</div>
 							</div>
 						</div>`);
-					});
-				}
-			},
 		});
-	});
+	};
+
+	const showErrorMessage = (
+		message = `Could not connect to server,
+		 please check your internet connection and try again.`
+	) => {
+		Swal.fire({
+			title: "Error!",
+			text: message,
+			icon: "error",
+			confirmButtonText: "Ok",
+			background: "#D9D9D9",
+			color: "#353535",
+			confirmButtonColor: "#284b63",
+		});
+	};
 };
 
 export default main;
