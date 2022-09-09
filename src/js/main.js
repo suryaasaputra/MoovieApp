84b0f7d366c1e2a418cbd413f7035132a25069e7
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-09-10 00:07:00 +0700

    update layout & search result not found

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import $ from "jquery";
 import moment from "moment";
 import "../component/hero.js";
+import image from "../img/empty.svg";
 const hero = $("#hero");
 const keyword = $("#keyword");
 const caption = $(".section-title");
+const mainPage = $("#main");
 
 const main = () => {
 	const $loading = $("#loadingDiv").hide();
@@ -51,35 +53,56 @@ const main = () => {
 		$.ajax({
 			url: `https://api.themoviedb.org/3/search/movie?api_key=a3d88eeb5f330c5238138a8a41be581e&language=en-US&query=${keyword.val()}&page=1&include_adult=false`,
 			success: (result) => {
-				hero.remove();
-				caption.text(`Hasil Pencarian ${keyword.val()}`);
-				$("#movieList").html("");
-				const searchResult = result.results;
-				$.each(searchResult, (num, movie) => {
-					const imgPath = "https://image.tmdb.org/t/p/original";
-					const dateConverted = moment(movie.release_date, "YYYY-MM-DD").format(
-						"LL"
+				console.log(result.results);
+				if (!result.results === []) {
+					console.log("search succes");
+					hero.remove();
+					caption.html(
+						`Search result for:<span class="keyword"> ${keyword.val()}</span>`
 					);
-					$("#movieList").append(`
-					<div class="col-lg-4 movie-item">
-						<div class="card movie_card">
-							<img
-								src="${imgPath}${movie.backdrop_path}"
-								class="card-img-top img-fluid"
-								alt="..."
-							/>
-						<div class="card-body">
-								<h5 class="card-title">${movie.title}</h5>
-								<p class="card-text">
-										${movie.overview}
-								</p>
-								<span class="movie_info">Release Date :<b> ${dateConverted}</b></span>
-								<span class="movie_info float-end" >Rate: <b>${movie.vote_average}/10</b></span>
-								
+					$("#movieList").html("");
+					const searchResult = result.results;
+					$.each(searchResult, (num, movie) => {
+						const imgPath = "https://image.tmdb.org/t/p/original";
+						const dateConverted = moment(
+							movie.release_date,
+							"YYYY-MM-DD"
+						).format("LL");
+						$("#movieList").append(`
+						<div class="col-lg-4 movie-item">
+							<div class="card movie_card">
+								<img
+									src="${imgPath}${movie.backdrop_path}"
+									class="card-img-top img-fluid"
+									alt="..."
+								/>
+							<div class="card-body">
+									<h5 class="card-title">${movie.title}</h5>
+									<p class="card-text">
+											${movie.overview}
+									</p>
+									<span class="movie_info">Release Date :<b> ${dateConverted}</b></span>
+									<span class="movie_info float-end" >Rate: <b>${movie.vote_average}/10</b></span>
+
+							</div>
+							</div>
+						</div>`);
+					});
+				} else {
+					console.log("Not Found");
+					hero.remove();
+					caption.html(
+						`Empty result for :<span class="keyword"> ${keyword.val()}</span>`
+					);
+					$("#movieList").html("");
+					mainPage.append(`
+					<div class="row py-5">
+						<div class="col-12 d-flex">
+							<img src=${image} class="img-fluid mx-auto"/>
 						</div>
-						</div>
-					</div>`);
-				});
+					</div>
+					`);
+				}
 			},
 		});
 	});
